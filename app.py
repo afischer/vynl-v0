692f16dd85c74a1e6e1b0893d8f7217f5d77581c
Andrew Fischer <Andrew@Howard-Fischers-Macbook-Pro.local>
2015-04-27 00:03:10 -0400

    wireframing

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/base")
+def base():
+    return render_template("base.html")
+
+
+
+
+
+if __name__ == "__main__":
+   app.debug = True
+   app.run(host="0.0.0.0", port=8000)
