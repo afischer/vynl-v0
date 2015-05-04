5fea6de9dd6e6e1d24ea1bb0ce81e844c0612e29
Zabari <zabari.daniel@Gmail.com>
2015-05-04 12:29:33 -0400

    finished one fxn

--- db.py
+++ db.py
@@ -1,10 +1,20 @@
 import sqlite3
-def addUnique(key):
+def addUnique(key):  # Adds unique key to uniques database, and creates a database with a unique name
 	conn = sqlite3.connect('uniques.db')
 	c=conn.cursor()
 	key=(key,)
 	try:
-		c.execute("CREATE TABLE unique (url text)",key)
+		c.execute("CREATE TABLE unique (url TEXT, active BOOLEAN)")
 	except:
 		pass
+	conn2=sqlite3.connect(key[0]+".db")
+	c2=conn2.cursor()
+	c2.execute("CREATE TABLE songs (videoid TEXT, upvotes REAL,downvotes REAL, name TEXT, artist TEXT, albumart TEXT)")
+	conn2.commit()
+	conn2.close()
+	c.execute("INSERT INTO unique (url) VALUES (?)", key)
+	conn.commit()
+	conn.close()
+
+
 
