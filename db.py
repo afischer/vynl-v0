b4a4f17c303931255cbfb11f90886c6e53a6e51a
Zabari <zabari.daniel@Gmail.com>
2015-05-07 12:34:10 -0400

    idk

--- db.py
+++ db.py
@@ -1,10 +1,11 @@
 import sqlite3
+import musicbrainzngs as mz
 def addUnique(key):  # Adds unique key to uniques database, and creates a database with a unique name
 	conn = sqlite3.connect('uniques.db')
 	c=conn.cursor()
 	key=(key,)
 	try:
-		c.execute("CREATE TABLE unique (url TEXT, active BOOLEAN)")
+		c.execute("CREATE TABLE unique (url TEXT, active INTEGER)")
 	except:
 		pass
 	conn2=sqlite3.connect(key[0]+".db")
@@ -15,10 +16,19 @@ def addUnique(key):  # Adds unique key to uniques database, and creates a databa
 	c.execute("INSERT INTO unique (url) VALUES (?)", key)
 	conn.commit()
 	conn.close()
-def addSong(key,url):
+
+def addSong(key,vid,image):
 	try:
 		conn=sqlite3.connect(key+'.db')
 	except:
-		print "key does not exist"
+		print "This party does not exist"
+		return -1
+	args=(vid,image,)
 	c=conn.cursor()
+	c.execute("INSERT INTO songs (videoid,albumart) VALUES (?,?)",args)
+	conn.commit()
+	conn.close()
+	
+	
+	
 	
