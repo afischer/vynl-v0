76b1b1a0708402cbad5fa940c93f757b1f37cce4
ezchen <echen2015@gmail.com>
2015-05-07 23:09:30 -0400

    created SearchSongRow class which defines a row in the dropdown

--- static/js/script.js
+++ static/js/script.js
@@ -1,103 +1,95 @@
 var apiKey = 'AIzaSyCotdvV8u6NXowT24nFcd1Tlh4XEn56nlg';
 
 function handleClientLoad() {
-        // Step 2: Reference the API key
     gapi.client.setApiKey(apiKey);
     gapi.client.load('youtube', 'v3', function() {});
-	console.log("youtube client loaded");
-}	
-
-function search(query) {
-		var q = query;
-		var request = gapi.client.youtube.search.list({
-			q: q,
-			part: 'snippet'
-		});
-
-  		request.execute(function(response) {
-    		var str = JSON.stringify(response.result);
-    		console.log(str);
-  		});
+    console.log("youtube client loaded");
 }
 
-    function doSearch(e)
-    {
-        results = [];
-
-        if (e != "")
-        {
-            $.each(searchJSON, function(t, n) {
-                var r = n.title, i = n.title, s = n.artist, o=n.album && results.push([r, s, o])
-            });
-
-            printResults();
-        }
-        else
-        {
-            $("#search_results").html();
-            results = [];
-            printResults();
-        }
-    }
-
-    function printResults()
-    {
-        var e = $("#search_results");
-
-        e.html("");
-
-        e.html(function() {
-            if (results.length == 0)
-            {
-                e.append('<li style="padding-top: 3px; padding-bottom: 3px"><a style="color: #999; word-wrap: break-word; white-space: normal" href="#">No results found</a></li>');
-            }
-            else
-            {
-                $.each(results, function(t, n) {
-                    e.append('<li style="padding-top: 3px; padding-bottom: 3px"><a style="color: #999; word-wrap: break-word; white-space: normal" href="' + n[1] + '">' + n[0] + '</a></li>');
-                });
-            }
-        });
-    }
-
-    // Show the dropdown menu as long as there are characters in the text field
-    function checkTextField()
-    {
-        // If the value of id search_input is not empty show id search_results otherwise hide it
-        if ($('#search_input').val() != '')
-        {
-            $('#search_results').show();
-        }
-        else
-        {
-            $('#search_results').hide();
-        }
-    }
-
-    // Hide the dropdown menu if there is a left mouse click outside of it
-    $(document).mouseup(function (e)
-    {
-        var container = $("#search_results");
-
-        // if the target of the click isn't the
-        // container nor a descendant of the container
-        if (!container.is(e.target) && container.has(e.target).length === 0)
-        {
-            container.hide();
-        }
+function search(query) {
+    var q = query;
+    var request = gapi.client.youtube.search.list({
+        q: q,
+        part: 'snippet'
     });
 
-    $(document).ready(function() {
-
-        // Continually update search results as characters are typed
-        $("#search_input").keyup(function() {
-            // Make search inputs are case insensitive
-            var e = $(this).val().toLowerCase();
-
-            // Do the actual search
-            search(e);
-        });
+    request.execute(function(response) {
+        var str = JSON.stringify(response.result);
+        console.log(str);
     });
+}
+
+var SearchSongRow = React.createClass({
+    render: function() {
+        var title = "test title";
+        var rowStyle = {
+            paddingTop: '3px',
+            paddingBottom: '3px'
+        };
+        return (
+            <li style={rowStyle}><a href="#">No results found</a></li>
+        );
+    }
+});
+
+function printResults()
+{
+    var e = $("#search_results");
+
+    e.html("");
+
+    e.html(function() {
+        if (results.length == 0)
+    {
+        e.append('<li style="padding-top: 3px; padding-bottom: 3px"><a style="color: #999; word-wrap: break-word; white-space: normal" href="#">No results found</a></li>');
+    }
+        else
+    {
+        $.each(results, function(t, n) {
+            e.append('<li style="padding-top: 3px; padding-bottom: 3px"><a style="color: #999; word-wrap: break-word; white-space: normal" href="' + n[1] + '">' + n[0] + '</a></li>');
+        });
+    }
+    });
+}
+
+// Show the dropdown menu as long as there are characters in the text field
+function checkTextField()
+{
+    // If the value of id search_input is not empty show id search_results otherwise hide it
+    if ($('#search_input').val() != '')
+    {
+        $('#search_results').show();
+    }
+    else
+    {
+        $('#search_results').hide();
+    }
+}
+
+// Hide the dropdown menu if there is a left mouse click outside of it
+$(document).mouseup(function (e)
+        {
+            var container = $("#search_results");
+
+            // if the target of the click isn't the
+            // container nor a descendant of the container
+            if (!container.is(e.target) && container.has(e.target).length === 0)
+{
+    container.hide();
+}
+});
+
+$(document).ready(function() {
+
+    // Continually update search results as characters are typed
+    $("#search_input").keyup(function() {
+        // Make search inputs are case insensitive
+        var e = $(this).val().toLowerCase();
+
+        // Do the actual search
+        search(e);
+    });
+});
 
 
 
@@ -111,32 +103,32 @@ $(function() {
     animateLine = function(canvas, DivName, colorNumber, pathString) {
         $('#' + DivName).hover(
 
-        function() {
-            var line = canvas.path(pathString).attr({
-                stroke: colorNumber,
-                // width:"10%",
-                // height:"10%",
-                // preserveAspectRatio:"x200Y200 meet",
-            });
-            var length = line.getTotalLength();
+            function() {
+                var line = canvas.path(pathString).attr({
+                    stroke: colorNumber,
+                    // width:"10%",
+                    // height:"10%",
+                    // preserveAspectRatio:"x200Y200 meet",
+                });
+                var length = line.getTotalLength();
 
-            $('path[fill*="none"]').animate({
-                'to': 1
-            }, {
-                duration: 3000,
-                step: function(pos, fx) {
-                    var offset = length * fx.pos;
-                    var subpath = line.getSubpath(0, offset);
-                    canvas.clear();
-                    canvas.path(subpath).attr({
-                        'stroke-width': 30,
-                        stroke: colorNumber,
-                        name: "svgLogo"
-                    });
+                $('path[fill*="none"]').animate({
+                    'to': 1
+                }, {
+                    duration: 3000,
+                    step: function(pos, fx) {
+                        var offset = length * fx.pos;
+                        var subpath = line.getSubpath(0, offset);
+                        canvas.clear();
+                        canvas.path(subpath).attr({
+                            'stroke-width': 30,
+                            stroke: colorNumber,
+                            name: "svgLogo"
+                        });
 
-                },
+                    },
+                });
             });
-        });
     };
 
     var canvas = Raphael('svg-canvas', w, h);
@@ -151,9 +143,9 @@ $(function() {
     });
 
     $(window).resize(function(){
-         w = "100%"
-         h = "100%"
-         canvas.setSize(w,h);
+        w = "100%"
+        h = "100%"
+        canvas.setSize(w,h);
     });
 
 });
