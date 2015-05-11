bc3f602a20e2fc1ec41be49e29b104acb9efae86
Zabari <zabari.daniel@Gmail.com>
2015-05-11 12:33:49 -0400

    close

--- db.py
+++ db.py
@@ -1,4 +1,4 @@
-import sqlite3
+import sqlite3,os
 import musicbrainzngs as mz
 class Party:
 	def __init__(self,key):  # Adds unique key to uniques database, and creates a database with a unique name
@@ -12,39 +12,66 @@ class Party:
 			pass
 		conn2=sqlite3.connect(self.db)
 		c2=conn2.cursor()
-		c2.execute("CREATE TABLE songs (videoid TEXT, upvotes REAL,downvotes REAL, name TEXT, artist TEXT, albumart TEXT)")
+		c2.execute("CREATE TABLE songs (videoid TEXT, upvotes REAL,downvotes REAL, name TEXT, artist TEXT, albumart TEXT,active INTEGER)")
 		conn2.commit()
 		conn2.close()
 		c.execute("INSERT INTO unique (url) VALUES (?)", (self.k,))
 		conn.commit()
 		conn.close()
+		self.active=True
 
 	def addSong(self,vid,image):
-		conn=sqlite3.connect(self.db)
-		args=(vid,image,)
-		c=conn.cursor()
-		c.execute("INSERT INTO songs (videoid,albumart) VALUES (?,?)",args)
-		conn.commit()
-		conn.close()
+		if self.active:
+			conn=sqlite3.connect(self.db)
+			args=(vid,image,1,)
+			c=conn.cursor()
+			c.execute("INSERT INTO songs (videoid,albumart,active) VALUES (?,?,?)",args)
+			conn.commit()
+			conn.close()
+		else:
+			print "Party not active."
+			return -1
 	
+	def removeSong(self,vid):
+		if self.active:
+			conn=sqlite3.connect(self.db)
+			c=conn.cursor()
+			c.execute("INSERT INTO songs (active) VALUES (?) WHERE videoid=?",(0,),(vid,))
+			conn.commit()
+			conn.close()
+		else:
+			print "Party not active."
+			return -1
 	def upVote(self,vid):
-		conn=sqlite3.connect(self.db)
-		c=conn.cursor()
-		num=c.execute("SELECT upvotes FROM songs WHERE videoid=?",(vid,))
-		num+=1
-		c.execute("INSERT INTO songs (upvotes) VALUES (?) WHERE videoid=?",(num,),(vid,))
-		conn.commit()
-		conn.close()
+		if self.active:
+			conn=sqlite3.connect(self.db)
+			c=conn.cursor()
+			num=c.execute("SELECT upvotes FROM songs WHERE videoid=?",(vid,))
+			num+=1
+			c.execute("INSERT INTO songs (upvotes) VALUES (?) WHERE videoid=?",(num,),(vid,))
+			conn.commit()
+			conn.close()
+        else:
+            print "Party not active."
+            return -1
 
-    	def downVote(self,vid):
-        	conn=sqlite3.connect(self.db)
-        	c=conn.cursor()
-        	num=c.execute("SELECT downvotes FROM songs WHERE videoid=?",(vid,))
-        	num+=1
-        	c.execute("INSERT INTO songs (downvotes) VALUES (?) WHERE videoid=?",(num,),(vid,))
-        	conn.commit()
-		conn.close()
 
-	
-	
+   	def downVote(self,vid):
+		if self.active:
+			conn=sqlite3.connect(self.db)
+			c=conn.cursor()
+			num=c.execute("SELECT downvotes FROM songs WHERE videoid=?",(vid,))
+			num+=1
+			c.execute("INSERT INTO songs (downvotes) VALUES (?) WHERE videoid=?",(num,),(vid,))
+			conn.commit()
+			conn.close()
+        else:
+            print "Party not active."
+            return -1
+
+	def end(self):
+		if self.active:
+			os.system('rm "'+self.db+'"')
+			self.active=False
+
 	
