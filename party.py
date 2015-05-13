1e873682824774a779c918321ec8d715f6c2e92a
Zabari <zabari.daniel@Gmail.com>
2015-05-13 12:34:31 -0400

    so close

--- party.py
+++ party.py
@@ -12,7 +12,7 @@ class Party:
 			pass
 		conn2=sqlite3.connect(self.db)
 		c2=conn2.cursor()
-		c2.execute("CREATE TABLE songs (videoid TEXT, upvotes REAL,downvotes REAL, name TEXT, artist TEXT, albumart TEXT,active INTEGER)")
+		c2.execute("CREATE TABLE songs (videoid TEXT, upvotes REAL,downvotes REAL, name TEXT, artist TEXT, albumart TEXT,active INTEGER,total REAL)")
 		conn2.commit()
 		conn2.close()
 		c.execute("INSERT INTO uniques (url,active) VALUES (?,?)", (self.k,1,))
@@ -25,7 +25,7 @@ class Party:
 			conn=sqlite3.connect(self.db)
 			args=(vid,image,1,title,)
 			c=conn.cursor()
-			c.execute("INSERT INTO songs (videoid,albumart,active,name) VALUES (?,?,?,?)",args)
+			c.execute("INSERT INTO songs (videoid,albumart,active,name,upvotes,downvotes,total) VALUES (?,?,?,?,0,0,0)",args)
 			conn.commit()
 			conn.close()
 		else:
@@ -46,9 +46,11 @@ class Party:
 		if self.active:
 			conn=sqlite3.connect(self.db)
 			c=conn.cursor()
-			num=c.execute("SELECT upvotes FROM songs WHERE videoid=?",(vid,)).fetchone()[0]
-			num+=1
-			c.execute("INSERT INTO songs (upvotes) VALUES (?) WHERE videoid=?",(num,),(vid,))
+			num=c.execute("SELECT (upvotes,total) FROM songs WHERE videoid=?",(vid,)).fetchone()
+			num[0]+=1
+			num[1]+=1
+			print num
+			c.execute("UPDATE songs SET upvotes=?, total=? WHERE videoid=?",(num[0],num[1],vid,))
 			conn.commit()
 			conn.close()
 		else:
@@ -62,7 +64,7 @@ class Party:
 			c=conn.cursor()
 			num=c.execute("SELECT downvotes FROM songs WHERE videoid=?",(vid,)).fetchone()[0]
 			num+=1
-			c.execute("INSERT INTO songs (downvotes) VALUES (?) WHERE videoid=?",(num,),(vid,))
+			c.execute("UPDATE songs SET downvotes=? WHERE videoid=?",(num,),(vid,))
 			conn.commit()
 			conn.close()
 		else:
@@ -75,11 +77,17 @@ class Party:
 			self.active=False
 			conn = sqlite3.connect('uniques.db')
 			c=conn.cursor()
-			c.execute("INSERT INTO uniques (active) VALUES (?) WHERE url=?",(0,),(self.k,))
-	def getOrdered(self):
+			c.execute("UPDATE uniques SET active=? WHERE url=?",(0,self.k,))
+			conn.commit()
+			conn.close()
+
+
+	def getOrdered(self): #{title: STRING, videoID: STRING, image: STRING, upvotes: int, downvotes: int}
 		if self.active:
+			ret=[]
 			conn=sqlite3.connect(self.db)
 			c=conn.cursor()
+			it=c.execute("SELECT (name,videoid, albumart, upvotes, downvotes) FROM songs WHERE active=1 SORT BY total")
 			
 
 	
---
+++ test.py
@@ -0,0 +1,9 @@
+import party,os
+#try:
+x=party.Party("hey")
+song="xxx5"
+x.addSong(song,"img","name")
+x.upVote(song)
+x.end()
+#except:
+#	os.system("rm uniques.db;rm hey.db")
