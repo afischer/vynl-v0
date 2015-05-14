2434c6938473d954202cc3a260242e63b3309c09
ezchen <echen2015@gmail.com>
2015-05-13 22:55:22 -0400

    fixed dropdown not showing when the input was focused in SearchSongDynamic

--- static/js/script.js
+++ static/js/script.js
@@ -5,9 +5,8 @@ function handleClientLoad() {
 }
 
 function search(query, callback) {
-    var q = query;
     var request = gapi.client.youtube.search.list({
-        q: q,
+        q: query,
         type: 'video',
         part: 'snippet'
     });
@@ -29,110 +28,122 @@ var SearchSongRow = React.createClass({
 });
 
 var SearchSongDropDown = React.createClass({
-	render: function() {
-		var rows = [];
-		this.props.songs.forEach(function(song) {
-			rows.push(<SearchSongRow song={song} key={song.snippet.title} />);
-		});
-		return (
-			<ul id={'search_results'} className={'dropdown-menu'} role={'menu'}>
-				{rows}
-			</ul>
-		);
-	}
+    render: function() {
+        var rows = [];
+        this.props.songs.forEach(function(song) {
+            rows.push(<SearchSongRow song={song} key={song.snippet.title} />);
+        });
+        return (
+            <ul id={'search_results'} className={'dropdown-menu'} role={'menu'}>
+            {rows}
+            </ul>
+        );
+    }
 });
 
 /*
-var SearchSongBar = React.createClass({
-	handleOnKeyUp: function() {
-		console.log("bad key up");
-	},
-	render: function() {
-		return (
-			<input onKeyUp={this.handleOnKeyUp} id={"search_input"} data-toggle={"dropdown"} type={"text"} className={"form-control"} placeholder={"Search"} autoFocus={"autofocus"} autoComplete={"off"} />
-		);
-	}
-});
-*/
+   var SearchSongBar = React.createClass({
+   handleOnKeyUp: function() {
+   console.log("bad key up");
+   },
+   render: function() {
+   return (
+   <input onKeyUp={this.handleOnKeyUp} id={"search_input"} data-toggle={"dropdown"} type={"text"} className={"form-control"} placeholder={"Search"} autoFocus={"autofocus"} autoComplete={"off"} />
+   );
+   }
+   });
+   */
 
 var SearchSongDynamic = React.createClass({
-	getInitialState: function() {
-		return {songs: [],
-			value: ''};
-	},
-	componentDidMount: function() {
-	},
-	componentWillUnmount: function() {
-	},
-	handleOnChange: function(event) {
-		this.setState({value: event.target.value});
+    getInitialState: function() {
+        return {
+            songs: [],
+            value: ''
+        };
+    },
+    handleOnFocus: function() {
+        if (this.state.value != null && this.state.value != '') {
+            $(React.findDOMNode(this.refs.searchSongDropDown)).show();
+        } else {
+            $(React.findDOMNode(this.refs.searchSongDropDown)).hide();
+        }
+    },
+    handleOnBlur: function() {
+        $(React.findDOMNode(this.refs.searchSongDropDown)).hide();
+    },
+    handleOnChange: function(event) {
+        this.setState({value: event.target.value});
 
-		var query = event.target.value;
-		search(query, function(response) {
-			console.log(response.items);
-			this.setState({songs: response.items});
-		}.bind(this));
-	},
-	render: function() {
-		var value = this.state.value;
-		return (
-			<div>
-				<input onChange={this.handleOnChange} value={value} id={"search_input"} data-toggle={"dropdown"} type={"text"} className={"form-control"} placeholder={"Search"} autoFocus={"autofocus"} autoComplete={"off"} />
-				<SearchSongDropDown songs={this.state.songs} />
-			</div>
-		);
-	}
+        var query = event.target.value;
+
+        if (query != null && query != '') {
+            search(query, function(response) {
+                console.log(response.items);
+                this.setState({songs: response.items});
+            }.bind(this));
+            $(React.findDOMNode(this.refs.searchSongDropDown)).show();
+        }
+    },
+    render: function() {
+        var value = this.state.value;
+        return (
+                <div>
+                    <input onChange={this.handleOnChange} onFocus={this.handleOnFocus} onBlur={this.handleOnBlur} value={value} id={"search_input"} data-toggle={"dropdown"} type={"text"} className={"form-control"} placeholder={"Search"} autoFocus={"autofocus"} autoComplete={"off"} />
+                    <SearchSongDropDown songs={this.state.songs} ref="searchSongDropDown" />
+                </div>
+                );
+    }
 });
 
 React.render(<SearchSongDynamic />, document.getElementById('SearchSongDynamic'));
 
 /*
-function printResults()
-{
-    var e = $("#search_results");
+   function printResults()
+   {
+   var e = $("#search_results");
 
-    e.html("");
+   e.html("");
 
-    e.html(function() {
-        if (results.length == 0)
-    {
-        e.append('<li style="padding-top: 3px; padding-bottom: 3px"><a style="color: #999; word-wrap: break-word; white-space: normal" href="#">No results found</a></li>');
-    }
-        else
-    {
-        $.each(results, function(t, n) {
-            e.append('<li style="padding-top: 3px; padding-bottom: 3px"><a style="color: #999; word-wrap: break-word; white-space: normal" href="' + n[1] + '">' + n[0] + '</a></li>');
-        });
-    }
-    });
-} */
+   e.html(function() {
+   if (results.length == 0)
+   {
+   e.append('<li style="padding-top: 3px; padding-bottom: 3px"><a style="color: #999; word-wrap: break-word; white-space: normal" href="#">No results found</a></li>');
+   }
+   else
+   {
+   $.each(results, function(t, n) {
+   e.append('<li style="padding-top: 3px; padding-bottom: 3px"><a style="color: #999; word-wrap: break-word; white-space: normal" href="' + n[1] + '">' + n[0] + '</a></li>');
+   });
+   }
+   });
+   } */
 
 /*
 // Show the dropdown menu as long as there are characters in the text field
 function checkTextField()
 {
-    // If the value of id search_input is not empty show id search_results otherwise hide it
-    if ($('#search_input').val() != '')
-    {
-        $('#search_results').show();
-    }
-    else
-    {
-        $('#search_results').hide();
-    }
+// If the value of id search_input is not empty show id search_results otherwise hide it
+if ($('#search_input').val() != '')
+{
+$('#search_results').show();
+}
+else
+{
+$('#search_results').hide();
+}
 } */
 
 /*
 // Hide the dropdown menu if there is a left mouse click outside of it
 $(document).mouseup(function (e)
-        {
-            var container = $("#search_results");
-
-            // if the target of the click isn't the
-            // container nor a descendant of the container
-            if (!container.is(e.target) && container.has(e.target).length === 0)
 {
-    container.hide();
+var container = $("#search_results");
+
+// if the target of the click isn't the
+// container nor a descendant of the container
+if (!container.is(e.target) && container.has(e.target).length === 0)
+{
+container.hide();
 }
 });
 */
