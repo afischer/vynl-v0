a7511eed83d305e65ee05c344662024344ef0c7f
ezchen <echen2015@gmail.com>
2015-05-24 11:03:06 -0400

    fixed indents

--- app.py
+++ app.py
@@ -8,24 +8,24 @@ app = Flask(__name__)
 
 @app.route("/api/parties/<party_id>/",methods=["GET","POST","PATCH","DELETE"])
 def apiParty(party_id):
-	newParty=p.Party(party_id)
-	if request.method == "GET":
+    newParty=p.Party(party_id)
+    if request.method == "GET":
         return jsonify(newParty.getOrdered())
-		#return jsonify({'hello':5,'hey':8})
+    #return jsonify({'hello':5,'hey':8})
     elif request.method == "POST":
         rec=request.get_json()
-		newParty.addSong(rec["videoID"],rec["title"],rec["artist"])
-	elif request.method== "PATCH":
-		rec=request.get_json()
-		if rec["upvote"]:
-			newParty.upVote(rec["videoID"])
-		else:
-			newParty.downVote(rec["videoID"])
-	elif request.method=="DELETE":
-		rec=request.get_json()
-		newParty.removeSong(rec["videoID"])
+        newParty.addSong(rec["videoID"],rec["title"],rec["artist"])
+    elif request.method== "PATCH":
+        rec=request.get_json()
+        if rec["upvote"]:
+            newParty.upVote(rec["videoID"])
+        else:
+            newParty.downVote(rec["videoID"])
+    elif request.method=="DELETE":
+        rec=request.get_json()
+        newParty.removeSong(rec["videoID"])
 
-	
+
 
 
 
@@ -43,7 +43,7 @@ def base():
 
 @app.route("/about")
 def about():
-  return render_template("about.html")
+    return render_template("about.html")
 
 @app.route("/party")
 def party():
@@ -51,19 +51,19 @@ def party():
 
 @app.route("/party/<partyID>")
 def genParty(partyID):
-   if (len(partyID)==8):
-      return render_template("party.html", partyID=partyID)
-   else:
-      return '<h1>404</h1>', 404
+    if (len(partyID)==8):
+        return render_template("party.html", partyID=partyID)
+    else:
+        return '<h1>404</h1>', 404
 
 @app.route("/<partyID>")
 def redirParty(partyID):
-   if (len(partyID)==8):
-      partyURL = "/party/" + partyID
-      return redirect(partyURL, code=303)
-   else:
-      return '<h1>404</h1>', 404
+    if (len(partyID)==8):
+        partyURL = "/party/" + partyID
+        return redirect(partyURL, code=303)
+    else:
+        return '<h1>404</h1>', 404
 
 if __name__ == "__main__":
-   app.debug = True
-   app.run(host="0.0.0.0", port=8000)
+    app.debug = True
+    app.run(host="0.0.0.0", port=8000)
