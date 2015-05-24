1610e47bc0e731659b14c38bffa1ca82ec93de49
ezchen <echen2015@gmail.com>
2015-05-24 13:50:04 -0400

    getSongs, addSong, and updateSong all work

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, redirect,request,jsonify, Response
+import json
 import random
 import string
 import party as p
@@ -6,57 +7,70 @@ import party as p
 app = Flask(__name__)
 
 
-@app.route("/api/parties/<party_id>/",methods=["GET","POST","PATCH","DELETE"])
+@app.route("/api/parties/<party_id>",
+           methods=["GET", "POST", "PATCH", "DELETE"])
 def apiParty(party_id):
-    newParty=p.Party(party_id)
+    newParty = p.Party(party_id)
     if request.method == "GET":
-        return jsonify(newParty.getOrdered())
-    #return jsonify({'hello':5,'hey':8})
+        return jsonify({"songs": newParty.getOrdered()})
     elif request.method == "POST":
-        rec=request.get_json()
-        newParty.addSong(rec["videoID"],rec["title"],rec["artist"])
-    elif request.method== "PATCH":
-        rec=request.get_json()
+        print "POST"
+        rec = request.get_json()
+        newParty.addSong(rec["videoID"], rec["title"], rec["artist"])
+        return jsonify({"success": True})
+    elif request.method == "PATCH":
+        rec = request.get_json()
         if rec["upvote"]:
             newParty.upVote(rec["videoID"])
         else:
             newParty.downVote(rec["videoID"])
-    elif request.method=="DELETE":
-        rec=request.get_json()
+        return jsonify({"success": True})
+    elif request.method == "DELETE":
+        rec = request.get_json()
+        print rec["videoID"]
         newParty.removeSong(rec["videoID"])
+
+
 def genID():
     return ''.join(random.choice(string.ascii_uppercase + string.digits) for i in range(8))
+
 
 @app.route("/")
 def index():
     return render_template("index.html", partyID=genID())
 
+
 @app.route("/base")
 def base():
     return render_template("base.html")
+
 
 @app.route("/about")
 def about():
     return render_template("about.html")
 
+
 @app.route("/party")
 def party():
     return render_template("party.html")
 
+
 @app.route("/parties/<partyID>")
 def genParty(partyID):
-    if (len(partyID)==8):
+    if (len(partyID) == 8):
         return render_template("party.html", partyID=partyID)
     else:
         return '<h1>404</h1>', 404
 
+
 @app.route("/<partyID>")
 def redirParty(partyID):
-   if (len(partyID)==8):
-      partyURL = "/parties/" + partyID
-      return redirect(partyURL, code=303)
-   else:
-      return '<h1>404</h1>', 404
+    if (len(partyID) == 8):
+        partyURL = "/parties/" + partyID
+        return redirect(partyURL, code=303)
+    else:
+        return '<h1>404</h1>', 404
+
 
 if __name__ == "__main__":
     app.debug = True
--- static/js/api.js
+++ static/js/api.js
@@ -2,30 +2,30 @@ var vynl = {
 };
 
 vynl.api = (function() {
-	var endpoint = "/api/";
-	
+	var endpoint = "/api";
+
 	var addSong = function(song, callback) {
 		$.ajax({
 			type: "POST",
 			url: endpoint + window.location.pathname,
-			contentType: "application/json",
+			contentType: "application/json; charset=utf-8",
 			dataType: "json",
-			data: song,
-			success: callback(data),
+			data: JSON.stringify(song),
+			success: callback,
 			error: function() {
 				console.log("error");
 			}
 		});
 	};
-	
+
 	var deleteSong = function(song, callback) {
 		$.ajax({
 			type: "DELETE",
 			url: endpoint + window.location.pathname,
 			contentType: "application/json",
 			dataType: "json",
-			data: song,
-			success: callback(data)
+			data: JSON.stringify(song),
+			success: callback
 		});
 	};
 
@@ -35,19 +35,25 @@ vynl.api = (function() {
 			url: endpoint + window.location.pathname,
 			contentType: "application/json",
 			dataType: "json",
-			data: song,
-			success: callback(data)
+			data: JSON.stringify(song),
+			success: callback
 		});
 	};
 
-	var getSongs = function(song, callback) {
+	var getSongs = function(callback) {
 		$.ajax({
 			type: "GET",
 			url: endpoint + window.location.pathname,
 			contentType: "application/json",
 			dataType: "json",
-			data: song,
-			success: callback(data)
+			success: callback
 		});
 	};
+
+        return {
+            addSong: addSong,
+            deleteSong: deleteSong,
+            updateSong: updateSong,
+            getSongs: getSongs
+        };
 }());
