fe56522394461125a94b48a5320ab64a97e43528
IsaacGluck <isaacbgluck@gmail.com>
2015-05-25 19:10:27 -0400

    baseline

--- app.py
+++ app.py
@@ -19,6 +19,10 @@ def base():
 def about():
   return render_template("about.html")
 
+@app.route("/contact")
+def contact():
+  return render_template("contact.html")
+
 @app.route("/party")
 def party():
     return render_template("party.html")
