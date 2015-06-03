e47d0348673c167fea6ad5741d0278555caec2d6
Daniel Zabari <zabari.daniel@Gmail.com>
2015-06-03 14:03:11 -0400

    Update party.py

--- party.py
+++ party.py
@@ -1,8 +1,9 @@
 import sqlite3
 import os
+import marshal as m
 
 def scrub(query):
-    return ''.join(x for x in query if x.isalnum())
+    return "["+''.join(x for x in query if x.isalnum())+"]"
 
 class Party:
     # Adds unique key to uniques database and creates a database with a name
@@ -20,14 +21,14 @@ class Party:
         #print self.active
         if (self.active):
             #print dj
-            self.dj=c.execute("SELECT ip FROM djs WHERE party=?",(self.k,)).fetchall()[0]
+            self.dj=c.execute("SELECT ip FROM djs WHERE party=?",(self.k,)).fetchall()[0][0]
             #print self.dj
             conn.close()
             return
         #print "why"
         #conn2=sqlite3.connect(self.db)
         #c2=conn2.cursor()
-        c.execute("CREATE TABLE " + self.k +"(videoid TEXT, imgURL TEXT, upvotes REAL, downvotes REAL, name TEXT, artist TEXT, active INTEGER,total REAL)")
+        c.execute("CREATE TABLE " + self.k +"(videoid TEXT, imgURL TEXT, upvotes REAL, downvotes REAL, name TEXT, artist TEXT, active INTEGER,total REAL, upvoteip BLOB, downvoteip BLOB)")
         conn.commit()
         self.addDJ(dj)
         c.execute("INSERT OR REPLACE INTO uniques (url,active) VALUES (?,?)", (self.k,1,))
@@ -39,9 +40,11 @@ class Party:
     def addSong(self,vid,imgURL,title,artist):
         if self.active:
             conn=sqlite3.connect(self.db)
-            args=(vid,imgURL,1,title,artist,)
+            x=m.dumps([])
+            y=m.dumps([])
+            args=(vid,imgURL,1,title,artist,x,y,)
             c=conn.cursor()
-            c.execute("INSERT INTO "+self.k+ "(videoid,imgURL,active,name,artist,upvotes,downvotes,total) VALUES (?,?,?,?,?,0,0,0)",args)
+            c.execute("INSERT INTO "+self.k+ "(videoid,imgURL,active,name,artist,upvotes,downvotes,total,upvoteip,downvoteip) VALUES (?,?,?,?,?,0,0,0,?,?)",args)
             conn.commit()
             conn.close()
         else:
@@ -58,12 +61,29 @@ class Party:
         else:
             print "Party not active."
             return -1
-    def upVote(self,vid):
+    def upVote(self,vid, ip):
         if self.active:
             conn=sqlite3.connect(self.db)
             c=conn.cursor()
-            num=c.execute("SELECT upvotes,total FROM "+self.k+" WHERE videoid=?",(vid,)).fetchone()
-            c.execute("UPDATE "+self.k+" SET upvotes=?, total=? WHERE videoid=?",(num[0]+1,num[1]+1,vid,))
+            num=c.execute("SELECT upvotes,total,upvoteip, downvotes, downvoteip FROM "+self.k+" WHERE videoid=?",(vid,)).fetchone()
+            x=m.loads(num[2])
+            y=m.loads(num[4])
+            if ip not in x:
+                if ip in y:
+                    x.append(ip)
+                    y.remove(ip)
+                    x=m.dumps(x)
+                    y=m.dumps(y)
+                    c.execute("UPDATE "+self.k+" SET upvotes=?, total=?, upvoteip=?, downvotes=?, downvoteip=? WHERE videoid=?",(num[0]+1,num[1]+2,x, num[3]-1,y,vid,))
+                else:
+                    x.append(ip)
+                    x=m.dumps(x)
+                    c.execute("UPDATE "+self.k+" SET upvotes=?, total=?, upvoteip=? WHERE videoid=?",(num[0]+1,num[1]+1,x,vid,))
+            elif ip in x:
+                x.remove(ip)
+                x=m.dumps(x)
+                c.execute("UPDATE "+self.k+" SET upvotes=?, total=?, upvoteip=? WHERE videoid=?",(num[0]-1,num[1]-1,x,vid,))
+
             conn.commit()
             conn.close()
         else:
@@ -71,12 +91,29 @@ class Party:
             return -1
 
 
-    def downVote(self,vid):
+    def downVote(self,vid, ip):
         if self.active:
             conn=sqlite3.connect(self.db)
             c=conn.cursor()
-            num=c.execute("SELECT downvotes,total FROM "+self.k+" WHERE videoid=?",(vid,)).fetchone()
-            c.execute("UPDATE "+self.k+" SET downvotes=?,total=? WHERE videoid=?",(num[0]+1,num[1]-1,vid,))
+            num=c.execute("SELECT downvotes,total,downvoteip, upvotes, upvoteip FROM "+self.k+" WHERE videoid=?",(vid,)).fetchone()
+            x=m.loads(num[2])
+            y=m.loads(num[4])
+            if ip not in x:
+                if ip in y:
+                    x.append(ip)
+                    y.remove(ip)
+                    x=m.dumps(x)
+                    y=m.dumps(y)
+                    c.execute("UPDATE "+self.k+" SET downvotes=?, total=?, downvoteip=?, upvotes=?, upvoteip=? WHERE videoid=?",(num[0]+1,num[1]-2,x, num[3]-1,y,vid,))
+                else:
+                    x.append(ip)
+                    x=m.dumps(x)
+                    c.execute("UPDATE "+self.k+" SET downvotes=?, total=?, downvoteip=? WHERE videoid=?",(num[0]+1,num[1]-1,x,vid,))
+            elif ip in x:
+                x.remove(ip)
+                x=m.dumps(x)
+                c.execute("UPDATE "+self.k+" SET downvotes=?, total=?, downvoteip=? WHERE videoid=?",(num[0]-1,num[1]+1,x,vid,))
+
             conn.commit()
             conn.close()
         else:
@@ -95,16 +132,17 @@ class Party:
             conn.close()
 
 
-    def getOrdered(self): #{title: STRING, artist:STRING, videoID: STRING, upvotes: int, downvotes: int}
+    def getOrdered(self,ip): #{title: STRING, artist:STRING, videoID: STRING, upvotes: int, downvotes: int}
         if self.active:
             ret=[]
             conn=sqlite3.connect(self.db)
             c=conn.cursor()
-            it=c.execute("SELECT name,artist,videoid,imgURL, upvotes, downvotes FROM "+self.k+" WHERE active=1 ORDER BY total DESC").fetchall()
+            it=c.execute("SELECT name,artist,videoid,imgURL, upvotes, downvotes,upvoteip,downvoteip FROM "+self.k+" WHERE active=1 ORDER BY total DESC").fetchall()
             conn.close()
             for x in it:
-                ret.append({"songname":str(x[0]),"songartist":str(x[1]),"songID":str(x[2]),"albumarturl":str(x[3]),"upvotes":x[4],"downvotes":x[5]})
+                ret.append({"songname":str(x[0]),"songartist":str(x[1]),"songID":str(x[2]),"albumarturl":str(x[3]),"upvotes":x[4],"downvotes":x[5],"upvoted":(ip in m.loads(x[6])), "downvoted":(ip in m.loads(x[7]))})
             return ret
+
     def addDJ(self,dj):
         conn=sqlite3.connect(self.db)
         c=conn.cursor()
@@ -116,3 +154,7 @@ class Party:
         conn.commit()
         conn.close()
         #print "ayy"
+
+    def getDJ(self):
+        if self.active:
+            return self.dj
