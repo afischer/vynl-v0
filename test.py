dd8b3741b9ebe1fcda7b2eb89afc0e353a736256
Daniel Zabari <zabari.daniel@Gmail.com>
2015-06-03 14:03:57 -0400

    Update test.py

--- test.py
+++ test.py
@@ -10,21 +10,22 @@ x.addSong(song2,"exampleimage","name2","channelwhat")
 x.addSong(song3,"exampleimage","name3","channelwho")
 x.addSong(song4,"exampleimage","name4","itstaytay")
 for i in xrange(20):
-	x.upVote(song)
-for i in xrange(10):
-	x.downVote(song)
+	x.upVote(song,"199")
+for i in xrange(11):
+	x.downVote(song,"167")
 for i in xrange(13):
-	x.upVote(song2)
+	x.upVote(song2,"189")
 for i in xrange(20):
-	x.downVote(song3)
+	x.downVote(song3,"155")
 for i in xrange(30):
-	x.downVote(song4)
+	x.downVote(song4,"155")
 for i in xrange(50):
-	x.upVote(song4)
+	x.upVote(song4,"155")
 
-print x.getOrdered()
+print x.getOrdered("155")
 y=party.Party("hey")
-print y.getOrdered()
+print y.getDJ()
+print y.getOrdered("167")
 x.end()
 #except:
 #	os.system("rm uniques.db;rm hey.db")
