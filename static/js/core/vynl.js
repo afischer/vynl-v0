04d24795d075ff1cbf12015a4ac7f23a70a9e16f
Andrew Fischer <afischer15@mac.com>
2015-07-26 21:53:00 -0400

    frontpage cleanup

--- static/js/core/vynl.js
+++ static/js/core/vynl.js
@@ -9,38 +9,36 @@
  * This module contains globally required Vynl functionality.
  *
  * @author AndrewFischer
- *
  * @version 1.0
  */
 
 // / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / 
 // Namespace
 
- (function() { Vynl = { version: "1.0" }; })();
+ (function() { vynl = { version: "1.0" }; })();
 
  // / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / /
  // Core
   
   
-/*
- * Logging
- */
-Vynl.log   = console.log();
-Vynl.info  = console.info();
-Vynl.warn  = console.warn();
-Vynl.error = "";
-
 
 /*
  * Debug mode configuration object.
  */
-Vynl.debug = function() {
-    return {
-        "on"   : function(ft) {
-        },
-        "off"   : function(ft) {
-        },
-        "isOn"  : function(ft) {
-        }
-    };
-}();
+
+
+/*
+ * Logging
+ */
+ vynl.logger = function() {
+    console.warn("LOGGER");
+    var printMsg = function(logger, prefix, messages) {
+         logger.apply(console, args);
+     };
+    
+     return {
+       "generate" : function(level, args) {
+          printMsg(logger, forLevel.toUpperCase(), arguments);
+       }
+    }
+ }();
